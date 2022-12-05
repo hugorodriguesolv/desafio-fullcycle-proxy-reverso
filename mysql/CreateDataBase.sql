fb32a679d279f0459bf85ff7eacf72d5b4b25045
Hugo Rodrigues <hugorod81@gmail.com>
2022-12-05 01:33:33 -0300

    Acesso a base de dados do desafio

--- mysql/CreateDataBase.sql
+++ mysql/CreateDataBase.sql
@@ -9,5 +9,3 @@ SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
-
-INSERT INTO People (Name) VALUES ('Hugo')
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,15 +1,41 @@
 'use strict';
 
 const express = require('express');
+const mysql = require('mysql');
 
 // Constants
 const PORT = 5000;
 const HOST = 'nodeserver';
 
 // App
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.get('/', (req, res) => {
-  res.send('Este é o desafio Full Cycle Proxy Reverso!');
+  
+  var nome = req.query.nome;
+  var query = "INSERT INTO People (Name) values ($1)";
+
+  dbClient.query(query, nome, function(err){
+    if(err)
+      throw err;
+    else {
+      console.log('Nome inserido com sucesso!')    ;
+      res.redirect('/');      
+      res.end();
+    }               
+  }); 
+
 });
 
 app.listen(PORT, HOST, () => {
